87dbae886d2cdbbd2b35cc89dc556b834bb2de63
Tobias <mail@totokaka.io>
2018-05-13 20:15:03 +0200

    Make CreateVenv extend DefaultTask

--- src/main/groovy/io/totokaka/gradle/pyenv/tasks/CreateVenv.groovy
+++ src/main/groovy/io/totokaka/gradle/pyenv/tasks/CreateVenv.groovy
@@ -1,17 +1,19 @@
 package io.totokaka.gradle.pyenv.tasks
 
 import io.totokaka.gradle.pyenv.Utils
+import org.gradle.api.Action
+import org.gradle.api.DefaultTask
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.AbstractExecTask
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
 
 /**
  * Gradle task for creating a Python venv virtual environment.
  *
  * This task depends on Python3, as the venv python modules is used.
  */
-class CreateVenv extends AbstractExecTask {
+class CreateVenv extends DefaultTask {
 
     /**
      * The directory python is found in.
@@ -29,15 +31,23 @@ class CreateVenv extends AbstractExecTask {
         super(CreateVenv)
         prefixProp = project.objects.property(File)
         targetProp = project.objects.property(File)
-
-        environment('PYTHONHOME', "${ -> prefixProp.get().getAbsolutePath()}")
-        executable("${ -> prefixProp.get().getAbsolutePath()}/bin/python")
-        args(['-m', 'venv', "${ -> targetProp.get().getAbsolutePath()}"])
     }
 
     static {
         Utils.dslify(CreateVenv, 'prefixProp', 'prefix')
         Utils.dslify(CreateVenv, 'targetProp', 'target')
     }
+    
+    @TaskAction
+    void exec() {
+        String prefix = prefixProp.get().getAbsolutePath()
+        String target = targetProp.get().getAbsolutePath()
+        project.exec({ spec ->
+            spec.getEnvironment().put('PYTHONHOME', prefix)
+            spec.setExecutable("$prefix/bin/python")
+            spec.setArgs(['-m', 'venv', target])
+        } as Action)
+    }
+
 
 }
